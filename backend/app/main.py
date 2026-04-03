8dd8162117ba9bbdcd67c457c8432325ed429016
Cheril Gedam <cherilgedam@gmail.com>
2026-04-03 19:25:45 +0530

    h1

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,7 +1,17 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from app.routes import patient, appointment, department, doctor
 
 app = FastAPI()
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # allow frontend
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(patient.router)
 app.include_router(appointment.router)

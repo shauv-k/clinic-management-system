94591bb626a5b32e08bc4fc491401586c2c1409a
shauv-k <shauvikgogoi2005@gmail.com>
2026-04-03 17:27:56 +0530

    added doctor by dept name endpouint

--- backend/app/routes/doctor.py
+++ backend/app/routes/doctor.py
@@ -4,10 +4,7 @@ from app.services import doctor_service
 router = APIRouter(prefix="/doctors", tags=["Doctors"])
 
 
-# -------------------------------
-# GET BY DEPARTMENT
-# -------------------------------
-@router.get("/by-department/{department_id}")
+@router.get("/by-department-id/{department_id}")
 def get_by_department(department_id: int):
     try:
         data = doctor_service.get_doctors_by_department(department_id)
@@ -19,3 +16,17 @@ def get_by_department(department_id: int):
 
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
+    
+
+@router.get("/by-department-name/{department_name}")
+def get_by_department_name(department_name: str):
+    try:
+        data = doctor_service.get_doctors_by_department_name(department_name)
+
+        if not data:
+            return {"message": "No doctors found for this department"}
+
+        return data
+
+    except Exception as e:
+        raise HTTPException(status_code=404, detail=str(e))
--- backend/app/services/doctor_service.py
+++ backend/app/services/doctor_service.py
@@ -2,7 +2,7 @@ from app.db import get_connection
 
 
 # -------------------------------
-# GET DOCTORS BY DEPARTMENT
+# GET DOCTORS BY DEPARTMENT ID
 # -------------------------------
 def get_doctors_by_department(department_id: int):
     conn = get_connection()
@@ -28,6 +28,54 @@ def get_doctors_by_department(department_id: int):
             "doctor_id": r[0],
             "name": r[1],
             "specialization": r[2],
+        }
+        for r in rows
+    ]
+
+# -------------------------------
+# GET DOCTORS BY DEPARTMENT NAME
+# -------------------------------
+def get_doctors_by_department_name(department_name: str):
+    conn = get_connection()
+    cursor = conn.cursor()
+
+    # Step 1: get department_id
+    cursor.execute("""
+        SELECT department_id
+        FROM DEPARTMENT
+        WHERE LOWER(name) = LOWER(:name)
+    """, {"name": department_name})
+
+    dept = cursor.fetchone()
+
+    if not dept:
+        cursor.close()
+        conn.close()
+        raise Exception("Department not found")
+
+    department_id = dept[0]
+
+    # Step 2: fetch doctors
+    cursor.execute("""
+        SELECT d.staff_id AS doctor_id,
+               s.name,
+               d.specialization
+        FROM DOCTOR d
+        JOIN STAFF s ON d.staff_id = s.staff_id
+        WHERE s.department_id = :department_id
+        ORDER BY s.name
+    """, {"department_id": department_id})
+
+    rows = cursor.fetchall()
+
+    cursor.close()
+    conn.close()
+
+    return [
+        {
+            "doctor_id": r[0],
+            "name": r[1],
+            "specialization": r[2]
         }
         for r in rows
     ]

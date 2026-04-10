8c451a77bc68d8468a37aa336b2e6eae217eb95f
shauv-k <shauvikgogoi2005@gmail.com>
2026-04-10 23:39:16 +0530

    added cors

--- backend/app/main.py
+++ backend/app/main.py
@@ -6,9 +6,9 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[""],  # allow frontend
+    allow_origins=["http://localhost:5173"],  
     allow_credentials=True,
-    allow_methods=[""],
+    allow_methods=["*"],
     allow_headers=["*"],
 )
 
